015dcd3331aa9e99724f44dc4ed2b3c12d18cf3d
Flynn Platt <flynn.platt@jpl.nasa.gov>
2016-06-13 16:06:30 -0700

    fixing stuff from the copy

--- postinstall.sh
+++ postinstall.sh
@@ -6,4 +6,5 @@ cp -r ./src/lib/cesium/* ./public
 
 
 rm -rf ./src/styles/lib && mkdir ./src/styles/lib
-cp ./node_modules/flexboxgrid/dist/flexboxgrid.min.css ./src/styles/lib/
+mkdir ./src/styles/lib/flexboxgrid/
+cp ./node_modules/flexboxgrid/dist/flexboxgrid.min.css ./src/styles/lib/flexboxgrid/
