596e36dd0ed762b0cd1c49a5502e9eaf0591befe
Flynn Platt <flynn.platt@jpl.nasa.gov>
2016-06-22 10:19:05 -0700

    adding tests

--- src/tests/MiscUtil.spec.js
+++ src/tests/MiscUtil.spec.js
@@ -1,42 +1,257 @@
 import { expect } from 'chai';
+import Immutable from 'immutable';
 import MiscUtil from '../utils/MiscUtil';
 
 describe('Misc Utils', () => {
     describe('generateStringFromSet', () => {
         it('returns a space separated string from a set of strings mapped to booleans', () => {
-            // our string set
-            let stringSet = {
+            let varIn = {
                 "foo": true,
                 "bar": false,
                 "fubar": true
             };
+            let varOut = 'foo fubar';
 
             //assert
-            expect(MiscUtil.generateStringFromSet(stringSet)).to.equal('foo fubar');
+            expect(MiscUtil.generateStringFromSet(varIn)).to.equal(varOut);
         });
 
         it('handles only string booleans (true/false)', () => {
-            // our string set
-            let stringSet = {
+            let varIn = {
                 "foo": 1,
                 "bar": true,
                 "fubar": "true"
             };
+            let varOut = 'bar';
 
             //assert
-            expect(MiscUtil.generateStringFromSet(stringSet)).to.equal('bar');
+            expect(MiscUtil.generateStringFromSet(varIn)).to.equal(varOut);
         });
 
         it('returns empty string if no strings map to true', () => {
-            // our string set
-            let stringSet = {
+            let varIn = {
                 "foo": false,
                 "bar": false,
                 "fubar": false
             };
+            let varOut = '';
 
             //assert
-            expect(MiscUtil.generateStringFromSet(stringSet)).to.equal('');
+            expect(MiscUtil.generateStringFromSet(varIn)).to.equal(varOut);
+        });
+    });
+    describe('findObjectInArray', () => {
+        it('returns the first object in an array that has a matching key/value pair', () => {
+            // DEFINE VARS
+            let varIn = [{
+                testkey: "testvalue",
+                indexA: true
+            }, {
+                testkey: "testvalue",
+                indexB: true
+            }, {
+                testkey: "testvalue",
+                indexC: true
+            }];
+            let varOut = varIn[0];
+
+            //assert
+            expect(MiscUtil.findObjectInArray(varIn, "testkey", "testvalue")).to.equal(varOut);
+        });
+        it('returns false if a matching object is not found', () => {
+            // DEFINE VARS
+            let varIn = [{
+                testkey: "testvalue",
+                indexA: true
+            }, {
+                testkey: "testvalue",
+                indexB: true
+            }, {
+                testkey: "testvalue",
+                indexC: true
+            }];
+            let varOut = false;
+
+            //assert
+            expect(MiscUtil.findObjectInArray(varIn, "indexD", true)).to.equal(varOut);
+        });
+    });
+    describe('findObjectWithIndexInArray', () => {
+        it('returns an object containing the first object in an array that has a matching key/value pair and its index in the array', () => {
+            // DEFINE VARS
+            let varIn = [{
+                testkey: "testvalue",
+                indexA: true
+            }, {
+                testkey: "testvalue",
+                indexB: true
+            }, {
+                testkey: "testvalue",
+                indexC: true
+            }];
+            let varOut = {
+                value: varIn[1],
+                index: 1
+            };
+
+            //assert
+            expect(MiscUtil.findObjectWithIndexInArray(varIn, "indexB", true)).to.deep.equal(varOut);
+        });
+        it('returns false if a matching object is not found', () => {
+            // DEFINE VARS
+            let varIn = [{
+                testkey: "testvalue",
+                indexA: true
+            }, {
+                testkey: "testvalue",
+                indexB: true
+            }, {
+                testkey: "testvalue",
+                indexC: true
+            }];
+            let varOut = false;
+
+            //assert
+            expect(MiscUtil.findObjectWithIndexInArray(varIn, "indexD", true)).to.equal(varOut);
+        });
+    });
+    describe('getImmutableObjectSort', () => {
+        it('takes a key and returns a sort function comparing that key for an Immutable List of Map objects.', () => {
+            // DEFINE VARS
+            let immutableArr = Immutable.fromJS([{
+                index: 4,
+                testkey: "testvalue"
+            }, {
+                index: 2,
+                testkey: "testvalue"
+            }, {
+                index: 1,
+                testkey: "testvalue"
+            }, {
+                index: 5,
+                testkey: "testvalue"
+            }, {
+                index: 3,
+                testkey: "testvalue"
+            }]);
+            let varIn = MiscUtil.getImmutableObjectSort("index");
+            let varOut = [{
+                index: 1,
+                testkey: "testvalue"
+            }, {
+                index: 2,
+                testkey: "testvalue"
+            }, {
+                index: 3,
+                testkey: "testvalue"
+            }, {
+                index: 4,
+                testkey: "testvalue"
+            }, {
+                index: 5,
+                testkey: "testvalue"
+            }];
+
+            //assert
+            expect(immutableArr.sort(varIn).toJS()).to.deep.equal(varOut);
+        });
+    });
+    describe('getHexFromColorString', () => {
+        it('returns a hex string from an rgb string with space separators', () => {
+            // DEFINE VARS
+            let varIn = "123 32 3";
+            let varOut = "#7B2003";
+
+            //assert
+            expect(MiscUtil.getHexFromColorString(varIn)).to.equal(varOut);
+        });
+
+        it('returns a hex string from an rgb string with comma separators', () => {
+            // DEFINE VARS
+            let varIn = "123,32,3";
+            let varOut = "#7B2003";
+
+            //assert
+            expect(MiscUtil.getHexFromColorString(varIn)).to.equal(varOut);
+        });
+        it('returns a hex string from an rgb string with comma space separators', () => {
+            // DEFINE VARS
+            let varIn = "123, 32, 3";
+            let varOut = "#7B2003";
+
+            //assert
+            expect(MiscUtil.getHexFromColorString(varIn)).to.equal(varOut);
+        });
+        it('returns a hex string from an rgb string with an rgb wrapper', () => {
+            // DEFINE VARS
+            let varIn = "rgb(123,32,3)";
+            let varOut = "#7B2003";
+
+            //assert
+            expect(MiscUtil.getHexFromColorString(varIn)).to.equal(varOut);
+        });
+        it('returns a capitalized hex string from a hex string', () => {
+            // DEFINE VARS
+            let varIn = "#7b2003";
+            let varOut = "#7B2003";
+
+            //assert
+            expect(MiscUtil.getHexFromColorString(varIn)).to.equal(varOut);
+        });
+        it('returns a # prefixed hex string from a hex number', () => {
+            // DEFINE VARS
+            let varIn = "7b2003";
+            let varOut = "#7B2003";
+
+            //assert
+            expect(MiscUtil.getHexFromColorString(varIn)).to.equal(varOut);
+        });
+    });
+    describe('parseUrlHashString', () => {
+        it('returns an array of objects representing the key/value pairs in a url format hash string', () => {
+            // DEFINE VARS
+            let varIn = "#a=b&c=d&e=f";
+            let varOut = [{
+                key: "a",
+                value: "b"
+            }, {
+                key: "c",
+                value: "d"
+            }, {
+                key: "e",
+                value: "f"
+            }];
+
+            //assert
+            expect(MiscUtil.parseUrlHashString(varIn)).to.deep.equal(varOut);
+        });
+        it('returns an array of objects representing the key/value pairs in a url format string without the preceeding hash', () => {
+            // DEFINE VARS
+            let varIn = "a=b&c=d,f,g&e=f";
+            let varOut = [{
+                key: "a",
+                value: "b"
+            }, {
+                key: "c",
+                value: "d,f,g"
+            }, {
+                key: "e",
+                value: "f"
+            }];
+
+            //assert
+            expect(MiscUtil.parseUrlHashString(varIn)).to.deep.equal(varOut);
+        });
+        it('ignores keys without values and values without keys', () => {
+            // DEFINE VARS
+            let varIn = "a=b&=c&e=&d";
+            let varOut = [{
+                key: "a",
+                value: "b"
+            }];
+
+            //assert
+            expect(MiscUtil.parseUrlHashString(varIn)).to.deep.equal(varOut);
         });
     });
 });
---
+++ src/tests/store.spec.js
@@ -0,0 +1,47 @@
+// import * as actionTypes from '../constants/actionTypes';
+// import { createStore } from 'redux';
+// import { expect } from 'chai';
+// import rootReducer from '../reducers';
+// import { mapState, layerModel, paletteModel } from '../reducers/models/map';
+// import { asyncState } from '../reducers/models/async';
+// import { helpContainerState } from '../reducers/models/helpContainer';
+// import { settingsContainerState } from '../reducers/models/settingsContainer';
+// import { viewState } from '../reducers/models/view';
+
+
+// const initialState = {
+//     map: mapState,
+//     view: viewState,
+//     async: asyncState,
+//     helpContainer: helpContainerState
+//     settingsContainer: settingsContainerState
+// };
+
+
+// describe('Store', function() {
+//     it('THIS IS A CRAZY TEST', function() {
+//         const store = createStore(rootReducer, initialState);
+
+//         const actions = [
+//             { type: actionTypes.OPEN_HELP },
+//             { type: actionTypes.CLOSE_HELP },
+//             { type: actionTypes.OPEN_HELP }
+//         ];
+//         actions.forEach(action => store.dispatch(action));
+
+//         const actual = store.getState();
+//         const expected = {
+//             map: mapState,
+//             view: viewState,
+//             async: asyncState,
+//             helpContainer: helpContainerState.set("isOpen", true)
+//             settingsContainer: settingsContainerState
+//         };
+
+//         expect(actual.map.toJS()).to.deep.equal(expected.map.toJS());
+//         expect(actual.view.toJS()).to.deep.equal(expected.view.toJS());
+//         expect(actual.async.toJS()).to.deep.equal(expected.async.toJS());
+//         expect(actual.helpContainer.toJS()).to.equal(expected.helpContainer.toJS());
+//         expect(actual.settingsContainer.toJS()).to.deep.equal(expected.settingsContainer.toJS());
+//     });
+// });
