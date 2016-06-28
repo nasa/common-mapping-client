e6f06d074d85d2751b95419ba688573c37ccc318
aplave <aaron.plave@jpl.nasa.gov>
2016-06-28 15:54:38 -0400

    Added mapskin copy from /lib to /styles/lib in postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -4,9 +4,17 @@
 rm -rf public && mkdir public
 cp -r ./src/lib/cesium/* ./public
 
-
+# Remove styles lib
 rm -rf ./src/styles/lib && mkdir ./src/styles/lib
+
+# Copy flexbox
 mkdir ./src/styles/lib/flexboxgrid/
 cp ./node_modules/flexboxgrid/dist/flexboxgrid.min.css ./src/styles/lib/flexboxgrid/
+
+# Copy normalize.css
 mkdir ./src/styles/lib/normalize/
 cp ./node_modules/normalize.css/normalize.css ./src/styles/lib/normalize/
+
+# Copy mapskin
+mkdir ./src/styles/lib/mapskin/
+cp -r ./src/lib/mapskin ./src/styles/lib/mapskin
