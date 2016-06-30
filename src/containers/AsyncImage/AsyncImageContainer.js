a8fe2943ffa59df2bf38d0a607a27b09298de078
aplave <aaron.plave@jpl.nasa.gov>
2016-06-30 17:39:25 -0400

    Created stateless AsyncImage container that can be used to load/fade in images. Does not address non-cached images like some tiles from GIBS. Closes #39.

---
+++ src/containers/AsyncImage/AsyncImageContainer.js
@@ -0,0 +1,65 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import ReactDOM from 'react-dom';
+import * as actions from '../../actions/AppActions';
+
+
+export class AsyncImageContainer extends Component {
+    onImageLoad() {
+        let imgLoader = ReactDOM.findDOMNode(this.refs.imgLoader);
+        let imgSrc = imgLoader.getAttribute('src');
+        let imgDest = ReactDOM.findDOMNode(this.refs.imgDest);
+        imgDest.style.backgroundImage = "url(" + imgSrc + ")";
+        imgDest.style.opacity = "1";
+        console.log("LOAD");
+    }
+    destroyLoader() {
+        let imgLoader = ReactDOM.findDOMNode(this.refs.imgLoader);
+        let imgDest = ReactDOM.findDOMNode(this.refs.imgDest);
+        imgLoader.onload = null;
+        imgLoader.src = "";
+        imgDest.style.backgroundImage = "";
+        console.log("DEST");
+    }
+    componentWillUnmount() {
+        this.destroyLoader();
+    }
+    componentDidMount() {
+        let imgLoader = ReactDOM.findDOMNode(this.refs.imgLoader);
+        let imgSrc = imgLoader.getAttribute('src');
+        // let img = new window.Image();
+        imgLoader.onload = () => { this.onImageLoad() };
+        imgLoader.src = imgSrc;
+    }
+    render() {
+        return (
+            <div>
+                <img style={{"display":"none"}} src={this.props.src} ref="imgLoader"></img>
+                <div style={{"transition":"opacity 0.3s"}} className={this.props.className} ref="imgDest"></div>
+            </div>
+        );
+    }
+}
+
+AsyncImageContainer.propTypes = {
+    actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        // helpOpen: state.help.get("isOpen"),
+        // helpPage: state.help.get("helpPage")
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(actions, dispatch)
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AsyncImageContainer);
--- src/containers/LayerInfo/LayerInfoContainer.js
+++ src/containers/LayerInfo/LayerInfoContainer.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Dialog from 'react-toolbox/lib/dialog';
+import AsyncImageContainer from '../AsyncImage/AsyncImageContainer';
 // import { Button, IconButton } from 'react-toolbox/lib/button';
 import { List, ListItem, ListSubHeader, ListCheckbox, ListDivider } from 'react-toolbox/lib/list';
 import * as actions from '../../actions/AppActions';
@@ -15,7 +16,8 @@ export class LayerInfoContainer extends Component {
                 active={this.props.isOpen}
                 onEscKeyDown={() => this.props.actions.closeLayerInfo()} 
                 onOverlayClick={() => this.props.actions.closeLayerInfo()}>
-                <div className="thumbnail-image" style={{backgroundImage: "url("+this.props.layer.get("thumbnailImage")+")"}}></div>
+                {/*<div className="thumbnail-image" style={{backgroundImage: "url("+this.props.layer.get("thumbnailImage")+")"}}></div>*/}
+                <AsyncImageContainer className="thumbnail-image" src={this.props.layer.get("thumbnailImage")}></AsyncImageContainer>
                 <div className="layerInfo-content">
                     <div className={metadata ? "" : "hidden"}>
                         <h2>{this.props.layer.get("title")}</h2>
