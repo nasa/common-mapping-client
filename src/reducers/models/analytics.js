35dcd58bb90170b06378b56610a0b78fd248e7fe
Flynn Platt <flynn.platt@jpl.nasa.gov>
2016-07-21 13:32:31 -0700

    changed default to false since we have no endpoint and 404s are annoying. Closes #13

--- src/reducers/models/analytics.js
+++ src/reducers/models/analytics.js
@@ -1,7 +1,7 @@
 import Immutable from 'immutable';
 
 export const analyticsState = Immutable.fromJS({
-    isEnabled: true,
+    isEnabled: false,
     currentBatch: [],
     timeLastSent: new Date(),
     currBatchNum: 0
