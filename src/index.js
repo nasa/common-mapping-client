2bf9cc9c0e059f09c034df82c9b854397a7b7835
Flynn Platt <flynn.platt@jpl.nasa.gov>
2016-09-09 17:41:20 -0700

    directory renaming, now all components are under Components.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import AppContainer from './containers/AppContainer';
+import AppContainer from './components/App//AppContainer';
 require('./favicon.ico');
 
 const store = configureStore();
