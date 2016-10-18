e7fc237a4419de10a8564505f1e6f693111a1235
Flynn Platt <flynn.platt@jpl.nasa.gov>
2016-10-17 17:58:33 -0700

    added zero for single digit days in cofnig

--- src/constants/appConfig.js
+++ src/constants/appConfig.js
@@ -27,7 +27,7 @@ export const YEAR_ARRAY = (() => {
     return yearArr;
 })();
 export const MONTH_ARRAY = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-export const DAY_ARRAY = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+export const DAY_ARRAY = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
 
 // Analytics
 export const ANALYTICS_ENDPOINT = "http://localhost:3050/analyticsEndpoint";
