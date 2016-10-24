c0cfd7edeab397fdd94037d60d920f3b19a77f91
Flynn Platt <flynn.platt@jpl.nasa.gov>
2016-10-24 10:08:51 -0700

    cleaning postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -9,24 +9,13 @@ cp -r ./src/lib/cesium/* ./public
 mkdir ./public/CesiumDrawHelper
 cp ./src/_core/styles/resources/img/CesiumDrawHelper/*.png ./public/CesiumDrawHelper
 
-# Remove styles lib
-# rm -rf ./src/styles/lib && mkdir ./src/styles/lib
-
 # Copy flexbox
-# mkdir ./src/styles/lib/flexboxgrid/
-# cp ./node_modules/flexboxgrid/dist/flexboxgrid.min.css ./src/styles/lib/flexboxgrid/
 mkdir ./src/lib/flexboxgrid/
 cp ./node_modules/flexboxgrid/dist/flexboxgrid.min.css ./src/lib/flexboxgrid/
 
 # Copy normalize.css
-# mkdir ./src/styles/lib/normalize/
-# cp ./node_modules/normalize.css/normalize.css ./src/styles/lib/normalize/
 mkdir ./src/lib/normalize/
 cp ./node_modules/normalize.css/normalize.css ./src/lib/normalize/
-
-# Copy mapskin
-# mkdir ./src/styles/lib/mapskin/
-# cp -r ./src/lib/mapskin ./src/styles/lib/
 
 # Copy mapskin into public
 cp -r ./src/lib/mapskin ./public
