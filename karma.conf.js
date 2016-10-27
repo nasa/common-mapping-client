330fa633a97fd477f34ff5b9306bac94af51b09d
aplave <aaron.plave@jpl.nasa.gov>
2016-10-27 10:35:03 -0700

    Running tests in phantomJS to see if using non-set up chrome browser for karma testing in travis was the cause of the unspecified test error.

--- karma.conf.js
+++ karma.conf.js
@@ -95,8 +95,9 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
         // browsers: ['PhantomJS','Chrome'],
-        browsers: ['Chrome'],
+        // browsers: ['Chrome'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
