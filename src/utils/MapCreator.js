f8137e83eeb28b2ff97b7451fe6c6fa31df17517
Flynn Platt <flynn.platt@jpl.nasa.gov>
2016-10-27 17:22:42 -0700

    adding in map creator util

---
+++ src/utils/MapCreator.js
@@ -0,0 +1,15 @@
+import * as appStrings from '_core/constants/appStrings';
+import MapWrapper_openlayers from '_core/utils/MapWrapper_openlayers';
+import MapWrapper_cesium from '_core/utils/MapWrapper_cesium';
+
+// creates a new object that abstracts a mapping library
+export function createMap(type, container, mapOptions) {
+    switch (type) {
+        case appStrings.MAP_LIB_2D:
+            return new MapWrapper_openlayers(container, mapOptions);
+        case appStrings.MAP_LIB_3D:
+            return new MapWrapper_cesium(container, mapOptions);
+        default:
+            return false;
+    }
+}
