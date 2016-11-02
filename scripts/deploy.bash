b27be276c76b0f85b07c84e9ca0831155931bd37
aplave <aaron.plave@jpl.nasa.gov>
2016-11-02 10:13:35 -0700

    Adding .nojekyll to deploy script to have gh-pages not ignore _ file/foldernames

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -3,7 +3,7 @@
 # Deploy script based on https://github.jpl.nasa.gov/M2020-CS3/m2020-app-template/blob/master/scripts/deploy.bash
 
 set -e          # Exit with nonzero exit code if anything fails
-# set -o verbose  # Print commands that are executed
+set -o verbose  # Print commands that are executed
 
 SOURCE_BRANCH=$TRAVIS_BRANCH
 TARGET_BRANCH="gh-pages"
@@ -12,9 +12,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "Skipping deployment because this is a PR merge build."
   exit 0
 fi
-
-echo "what's here"
-ls -la
 
 if [ ! -d "dist" ]; then
   echo "The dist/ directory doesn't exist; you must \`npm run build\` before deploying."
@@ -35,18 +32,32 @@ git remote set-branches --add origin $TARGET_BRANCH
 (git fetch origin $TARGET_BRANCH && git checkout -t origin/$TARGET_BRANCH) \
   || git checkout -b $TARGET_BRANCH # In case the gh-pages branch didn't exist before
 
+echo "what's here"
+ls -la
+
 # Clean out existing contents
 rm -rf $SOURCE_BRANCH
 
 # Rename the dist directory to match the source branch name
 mv dist $SOURCE_BRANCH
 
+echo "what's here now"
+ls -la
+
 # Move coverage output into source branch
 mv coverage $SOURCE_BRANCH/code-coverage
+
+echo "What's in source branch"
+ls $SOURCE_BRANCH -la 
 
 # Move public folders into root of app
 mv public/* $SOURCE_BRANCH
 git add --all -- $SOURCE_BRANCH
+
+git status
+
+# Add .nojekyll file to tell gh-pages not to use jekyll so that we can use _ in file/folder names
+touch .nojekyll
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 set +e
