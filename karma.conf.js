71a13a3b0d2a00d62ce63e8522c2f50aa2f0649d
aplave <aaron.plave@jpl.nasa.gov>
2016-11-07 10:39:54 -0800

    Bumping some karma browserdisconnect options to see if that solves the travis issue. Could be resource constraints causing the failure here.

--- karma.conf.js
+++ karma.conf.js
@@ -75,6 +75,10 @@ module.exports = function(config) {
             noInfo: true
         },
 
+        browserNoActivityTimeout: 60000,
+        browserDisconnectTolerance: 1,
+        browserDisconnectTimeout: 20000,
+
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
