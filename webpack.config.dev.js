2c6dea96857ce9f5ebf96522f4a329f5bdb4a826
Flynn Platt <flynn.platt@jpl.nasa.gov>
2016-12-06 09:06:18 -0800

    added direct loading for json files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -45,7 +45,7 @@ export default {
             { test: /(\.css|\.scss)$/, exclude: path.join(__dirname, 'node_modules/react-toolbox'), loaders: ['style', 'css?sourceMap', 'sass?sourceMap'] },
             { test: /(\.css|\.scss)$/, include: path.join(__dirname, 'node_modules/react-toolbox'), loaders: ['style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass?sourceMap!toolbox'] },
             { test: /\.(eot|woff|woff2|ttf|svg|gif|png|jpe?g)$/, loader: 'file-loader?name=img/[name].[ext]' },
-            { test: /\.md$/, loader: 'raw-loader' }
+            { test: /\.md|\.json$/, loader: 'raw-loader' }
         ]
     },
     toolbox: { theme: path.join(__dirname, 'src/styles/_theme.scss') }
