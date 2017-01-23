a53f44f0196413a7632f88457dac0e97c995be0e
aplave <aaron.plave@jpl.nasa.gov>
2017-01-23 13:10:01 -0800

    Copying over @fplatt work on issue #72.

--- src/components/App/AppContainer.js
+++ src/components/App/AppContainer.js
@@ -1,4 +1,14 @@
-// This is a stub file for a non-core application AppContainer. 
-// To begin, you should try copying over the contents of 
-// src/_core/components/App/AppContainer.js and modifying it
-// with your own components.
+/*********************/
+/*    APP CONTAINER  */
+/*********************/
+
+/*
+This is the main component container for your
+application. It will instantiate all the major
+sub-components of the application and perform
+any startup operations required.
+Create your own:
+1. Copy the contents of `src/_core/components/App/AppContainer.js` here
+2. Modify as needed with your custom components
+3. Modify `src/index.js` to use this AppContainer instead of the _core version
+*/
