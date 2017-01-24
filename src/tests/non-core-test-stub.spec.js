4eb1a2d2b2d24cf38c020803cd7cbb4e98ada9eb
aplave <aaron.plave@jpl.nasa.gov>
2017-01-24 14:22:31 -0800

    Made a non-core test stub file with a commented out normal Mocha test. Work on #90.

---
+++ src/tests/non-core-test-stub.spec.js
@@ -0,0 +1,32 @@
+// This is a stub file for non-Core Application tests.
+// Your tests should look something like this:
+
+
+// import { expect } from 'chai';
+// import MyUtils from 'utils/MyUtils';
+
+// const myUtils = new MyUtils();
+
+// describe('My Utils', () => {
+//     describe('generateHappyCat', () => {
+//         it('returns true after generating a happy cat', () => {
+//             let varIn = {
+//                 "yes": true
+//             };
+//             let varOut = true;
+
+//             //assert
+//             expect(MyUtils.generateHappyCat(varIn)).to.equal(varOut);
+//         });
+//         it('returns false after generating a non happy cat', () => {
+//             let varIn = {
+//                 "no": true
+//             };
+//             let varOut = false;
+
+//             //assert
+//             expect(MyUtils.generateHappyCat(varIn)).to.equal(varOut);
+//         });
+//     });
+//     describe('generateGrumpyCat', () => {})
+// })
