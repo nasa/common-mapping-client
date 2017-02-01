9d135252d1cbc2e5f23762d7c512729b7da36ba6
Flynn Platt <flynn.platt@jpl.nasa.gov>
2017-02-01 12:17:50 -0800

    updated config help text, removed unused domnode from index

--- src/config.js
+++ src/config.js
@@ -9,6 +9,9 @@ directly in index.html, without going through webpack.
 
 see `src/constants/appConfig.js` for all configuration options
 
+Note that configuration settings that are not expected to change
+during deployment should be made in `src/constants/appConfig.js` directly
+
 EXAMPLE:
 The following configuration will change the display title for
 the application.
