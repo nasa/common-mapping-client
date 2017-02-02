77fe896dce9143e4cb750a96714d927a700f0bb3
Flynn Platt <Flynn.Platt@jpl.nasa.gov>
2017-02-02 12:20:28 -0800

    adjusted alert severity levels. Closes #128 (#130)

--- src/_core/constants/appStrings.js
+++ src/_core/constants/appStrings.js
@@ -96,27 +96,27 @@ export const ALERTS = {
     INITIAL_DATA_LOAD_FAILED: {
         title: "Data Loading Failed",
         formatString: "Failed to load initial data for this application.",
-        severity: 5
+        severity: 3
     },
     URL_CONFIG_FAILED: {
         title: "Loading from URL Failed",
         formatString: "Failed to load application state configuration from url parameters.",
-        severity: 4
+        severity: 2
     },
     LAYER_ACTIVATION_FAILED: {
         title: "Layer Activation Failed",
         formatString: "Activating {LAYER} on the {MAP} map failed.",
-        severity: 5
+        severity: 2
     },
     BASEMAP_UPDATE_FAILED: {
         title: "Basemap Update Failed",
         formatString: "Activating {LAYER} as the basemap on the {MAP} map failed.",
-        severity: 3
+        severity: 2
     },
     LAYER_OPACITY_CHANGE_FAILED: {
         title: "Layer Opacity Change Failed",
         formatString: "Setting opacity for {LAYER} failed.",
-        severity: 3
+        severity: 2
     },
     GEOMETRY_SYNC_FAILED: {
         title: "Geometry Sync Failed",
@@ -126,17 +126,17 @@ export const ALERTS = {
     GEOMETRY_REMOVAL_FAILED: {
         title: "Shape Removal Failed",
         formatString: "Removal of all shapes from the {MAP} map failed.",
-        severity: 3
+        severity: 2
     },
     MEASUREMENT_REMOVAL_FAILED: {
         title: "Measurement Removal Failed",
         formatString: "Removal of all measurements from the {MAP} map failed.",
-        severity: 3
+        severity: 2
     },
     VIEW_MODE_CHANGE_FAILED: {
         title: "Map View Mode Change Failed",
         formatString: "Changing of map view mode to {MAP_VIEW_MODE} failed.",
-        severity: 3
+        severity: 2
     },
     VIEW_SYNC_FAILED: {
         title: "Setting Map Extent Failed",
@@ -146,17 +146,17 @@ export const ALERTS = {
     SET_DATE_FAILED: {
         title: "Date Update Failed",
         formatString: "Setting the date in the {MAP} map failed.",
-        severity: 4
+        severity: 2
     },
     CREATE_MAP_FAILED: {
         title: "Map Creation Failed",
         formatString: "The {MAP} map failed to initialize.",
-        severity: 5
+        severity: 3
     },
     FETCH_METADATA_FAILED: {
         title: "Loading Metadata Failed",
         formatString: "Could not retrieve metadata for {LAYER}.",
-        severity: 3
+        severity: 1
     }
 };
 /* END ALERTS */
