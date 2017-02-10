be4d1c0ed532447c85879da213131df69d7a1c32
Flynn Platt <flynn.platt@jpl.nasa.gov>
2017-02-09 18:01:05 -0800

    added docker deploy stuff

---
+++ scripts/dockerDeploy.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SOURCE_BRANCH=$BRANCH_NAME
+
+if [ ! -d "dist" ]; then
+  echo "The dist/ directory doesn't exist; you must \`npm run build\` before deploying."
+  exit 1
+fi
+
+# Make the branches dir if it doesn't exist
+if [ ! -d "branches" ]; then
+  mkdir branches
+fi
+
+# Move the built bundle into branches
+rm -rf branches/$SOURCE_BRANCH && mv dist branches/$SOURCE_BRANCH
+
+# copy over test results if they exist
+if [ -d "test-results" ]; then
+  mv test-results branches/$SOURCE_BRANCH/test-results
+  if [ -d "coverage" ]; then
+    mv coverage branches/$SOURCE_BRANCH/test-results/coverage
+  fi
+fi
+
+
+# clear old docker images and containers
+sudo docker ps | awk '{ print $1,$2 }' | grep jenkins/cmc-core | awk '{print $1 }' | xargs -I {} sudo docker stop {}
+sudo docker ps -a | awk '{ print $1,$2 }' | grep jenkins/cmc-core | awk '{print $1 }' | xargs -I {} sudo docker rm {}
+sudo docker images | awk '{ print $1,$2 }' | grep jenkins/cmc-core | awk '{print $1":"$2}' | xargs -I {} sudo docker rmi {}
+
+# build the new container
+sudo docker build -t jenkins/cmc-core -f scripts/deployAssets/Dockerfile .
+
+# run the new container
+sudo docker run -d -p 49160:80 --name cmc-core_auto-deploy jenkins/cmc-core;
