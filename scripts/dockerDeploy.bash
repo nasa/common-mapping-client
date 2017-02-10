9b1e10beef8afb8d2fb326632cd3d2fe88407d39
Flynn Platt <flynn.platt@jpl.nasa.gov>
2017-02-09 18:26:47 -0800

    added logs to dockerDeploy and updated dockerfile

--- scripts/dockerDeploy.bash
+++ scripts/dockerDeploy.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SOURCE_BRANCH=$BRANCH_NAME
+SOURCE_BRANCH=${BRANCH_NAME}
 
 if [ ! -d "dist" ]; then
   echo "The dist/ directory doesn't exist; you must \`npm run build\` before deploying."
@@ -8,29 +8,38 @@ if [ ! -d "dist" ]; then
 fi
 
 # Make the branches dir if it doesn't exist
+echo "Checking branches dir"
 if [ ! -d "branches" ]; then
   mkdir branches
 fi
 
 # Move the built bundle into branches
+echo "Creating target branch directory"
 rm -rf branches/$SOURCE_BRANCH && mv dist branches/$SOURCE_BRANCH
 
 # copy over test results if they exist
 if [ -d "test-results" ]; then
+  echo "Copying test results"
   mv test-results branches/$SOURCE_BRANCH/test-results
   if [ -d "coverage" ]; then
+    echo "Copying test coverage"
     mv coverage branches/$SOURCE_BRANCH/test-results/coverage
   fi
 fi
 
 
 # clear old docker images and containers
-sudo docker ps | awk '{ print $1,$2 }' | grep jenkins/cmc-core | awk '{print $1 }' | xargs -I {} sudo docker stop {}
-sudo docker ps -a | awk '{ print $1,$2 }' | grep jenkins/cmc-core | awk '{print $1 }' | xargs -I {} sudo docker rm {}
+echo "Clearing old containers and images"
+sudo docker ps | awk '{ print $1,$2 }' | grep cmc-core_auto-deploy | awk '{print $1 }' | xargs -I {} sudo docker stop {}
+sudo docker ps -a | awk '{ print $1,$2 }' | grep cmc-core_auto-deploy | awk '{print $1 }' | xargs -I {} sudo docker rm {}
 sudo docker images | awk '{ print $1,$2 }' | grep jenkins/cmc-core | awk '{print $1":"$2}' | xargs -I {} sudo docker rmi {}
 
 # build the new container
+echo "Building new image"
 sudo docker build -t jenkins/cmc-core -f scripts/deployAssets/Dockerfile .
 
 # run the new container
+echo "Starting container"
 sudo docker run -d -p 49160:80 --name cmc-core_auto-deploy jenkins/cmc-core;
+
+echo "Deploy complete"
