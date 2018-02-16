e2a491d257f84aba24397890ea143138308a0535
Flynn Platt <flynn.platt@gmail.com>
2018-02-15 17:32:07 -0800

    broke apart MapWrapper Initialization pieces to make overriding easier (#20)

--- src/_core/utils/MapWrapperCesium.js
+++ src/_core/utils/MapWrapperCesium.js
@@ -35,26 +35,60 @@ export default class MapWrapperCesium extends MapWrapper {
     constructor(container, options) {
         super(container, options);
 
+        this.init(container, options);
+    }
+
+    /**
+     * Initialize instance variables
+     *
+     * @param {string|domnode} container the container to render this map into
+     * @param {object} options view options for constructing this map wrapper (usually map state from redux)
+     * @memberof MapWrapperCesium
+     */
+    init(container, options) {
+        this.initBools(container, options);
+        this.initStaticClasses(container, options);
+        this.initObjects(container, options);
+
+        this.initializationSuccess = this.map ? true : false;
+    }
+
+    /**
+     * Initialize boolean values for this instance
+     *
+     * @param {string|domnode} container the container to render this map into
+     * @param {object} options view options for constructing this map wrapper (usually map state from redux)
+     * @memberof MapWrapperCesium
+     */
+    initBools(container, options) {
         this.is3D = true;
         this.isActive = options.getIn(["view", "in3DMode"]);
+    }
+
+    /**
+     * Initialize static class references for this instance
+     *
+     * @param {string|domnode} container the container to render this map into
+     * @param {object} options view options for constructing this map wrapper (usually map state from redux)
+     * @memberof MapWrapperCesium
+     */
+    initStaticClasses(container, options) {
         this.tileHandler = TileHandler;
         this.mapUtil = MapUtil;
         this.miscUtil = MiscUtil;
 
-        // Create cesium scene
-        window.CESIUM_BASE_URL = "assets/cesium";
-        this.cesium = window.Cesium;
+        this.initCesium(container, options);
+    }
 
-        // handle multiple initializations of DrawHelper
-        if (typeof window.DrawHelper.isPrepped === "undefined") {
-            window.DrawHelper = window.DrawHelper();
-            window.DrawHelper.isPrepped = true;
-        }
-        this.drawHelper = window.DrawHelper;
-
+    /**
+     * Initialize object instances for this instance
+     *
+     * @param {string|domnode} container the container to render this map into
+     * @param {options} options
+     * @memberof MapWrapperCesium
+     */
+    initObjects(container, options) {
         this.map = this.createMap(container, options);
-
-        this.initializationSuccess = this.map ? true : false;
 
         // Only continue if map was created
         if (this.map) {
@@ -78,6 +112,27 @@ export default class MapWrapperCesium extends MapWrapper {
             };
         }
     }
+
+    /**
+     * Initialize Cesium references for this instance
+     *
+     * @param {string|domnode} container the container to render this map into
+     * @param {object} options view options for constructing this map wrapper (usually map state from redux)
+     * @memberof MapWrapperCesium
+     */
+    initCesium(container, options) {
+        // Create cesium scene
+        window.CESIUM_BASE_URL = "assets/cesium";
+        this.cesium = window.Cesium;
+
+        // handle multiple initializations of DrawHelper
+        if (typeof window.DrawHelper.isPrepped === "undefined") {
+            window.DrawHelper = window.DrawHelper();
+            window.DrawHelper.isPrepped = true;
+        }
+        this.drawHelper = window.DrawHelper;
+    }
+
     /**
      * create a cesium map object
      *
