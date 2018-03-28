d2751c0428902c67404ee05b6fb5232fdcfb7dca
Flynn Platt <flynn.platt@gmail.com>
2018-03-28 15:53:42 -0700

    added hover color preview to json colorbars (#53)

--- src/_core/components/Colorbar/Colorbar.js
+++ src/_core/components/Colorbar/Colorbar.js
@@ -13,12 +13,28 @@ import * as appStrings from "_core/constants/appStrings";
 import styles from "_core/components/Colorbar/Colorbar.scss";
 
 export class Colorbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.colorPreviewValid = false;
+        this.colorPreview = {};
+    }
+
     renderColorbar() {
         switch (this.props.handleAs) {
             case appStrings.COLORBAR_JSON_FIXED:
             // falls through
             case appStrings.COLORBAR_JSON_RELATIVE:
-                return <ColorbarJSON palette={this.props.palette} />;
+                return (
+                    <ColorbarJSON
+                        palette={this.props.palette}
+                        handleAs={this.props.handleAs}
+                        min={this.props.min}
+                        max={this.props.max}
+                        onMouseOver={val => this.handleMouseOver(val)}
+                        onMouseOut={val => this.handleMouseOut()}
+                    />
+                );
             case appStrings.COLORBAR_IMAGE:
                 return <ColorbarImage url={this.props.url} />;
             default:
@@ -30,15 +46,45 @@ export class Colorbar extends Component {
         }
     }
 
+    handleMouseOver(val) {
+        this.colorPreviewValid = true;
+        this.colorPreview = val;
+        this.forceUpdate();
+    }
+
+    handleMouseOut() {
+        this.colorPreviewValid = false;
+        this.forceUpdate();
+    }
+
     renderRange() {
         if (this.props.handleAs) {
-            return (
-                <div className={styles.labelContainer}>
-                    <span className={styles.min}>{this.props.displayMin || this.props.min}</span>
-                    <span className={styles.units}>{this.props.units}</span>
-                    <span className={styles.max}>{this.props.displayMax || this.props.max}</span>
-                </div>
-            );
+            if (!this.colorPreviewValid) {
+                return (
+                    <div className={styles.labelContainer}>
+                        <span className={styles.min}>
+                            {this.props.displayMin || this.props.min}
+                        </span>
+                        <span className={styles.units}>{this.props.units}</span>
+                        <span className={styles.max}>
+                            {this.props.displayMax || this.props.max}
+                        </span>
+                    </div>
+                );
+            } else {
+                return (
+                    <div className={styles.colorPreview}>
+                        <span
+                            className={styles.color}
+                            style={{
+                                backgroundColor: this.colorPreview.color
+                            }}
+                        />
+                        <span className={styles.value}>{this.colorPreview.value}</span>
+                        <span className={styles.units}>{this.props.units}</span>
+                    </div>
+                );
+            }
         } else {
             return <div />;
         }
--- src/_core/components/Colorbar/ColorbarJSON.js
+++ src/_core/components/Colorbar/ColorbarJSON.js
@@ -8,17 +8,85 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import MiscUtil from "_core/utils/MiscUtil";
+import * as appStrings from "_core/constants/appStrings";
+import styles from "_core/components/Colorbar/Colorbar.scss";
 
 const CANVAS_WIDTH = 255;
 const CANVAS_HEIGHT = 12;
 
 export class ColorbarJSON extends Component {
-    componentDidMount() {
-        this.draw();
+    constructor(props) {
+        super(props);
+
+        this.listenersInitialized = false;
     }
 
-    componentDidUpdate(nextProps, nextState) {
+    componentDidMount() {
         this.draw();
+
+        this.initListeners();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.palette !== this.props.palette) {
+            this.draw();
+            this.initListeners();
+        }
+    }
+
+    initListeners() {
+        if (!this.listenersInitialized && this.props.palette && this.props.onMouseOver) {
+            // add the mouse over listener
+            let canvas = this.canvasNode;
+            canvas.addEventListener("mousemove", evt => {
+                let val = this.getValueFromX(evt.clientX);
+                if (val) {
+                    this.props.onMouseOver(val);
+                }
+            });
+
+            if (this.props.onMouseOut) {
+                canvas.addEventListener("mouseout", evt => {
+                    this.props.onMouseOut();
+                });
+            }
+
+            this.listenersInitialized = true;
+        }
+    }
+
+    getValueFromX(x) {
+        let canvas = this.canvasNode;
+        let canvasDim = canvas.getBoundingClientRect();
+        let canvasX = canvasDim.left;
+        x = x - canvasX;
+        let min = 0;
+        let max = CANVAS_WIDTH - 1;
+        if (x >= min && x <= max) {
+            let listSize = this.props.palette.get("values").size;
+            let offsetPercent = x / CANVAS_WIDTH;
+            let offset = Math.min(Math.round(offsetPercent * listSize), listSize - 1);
+            let value = this.props.palette.getIn(["values", offset]);
+            let val = value.get("value");
+            let colorStr = value.get("color");
+
+            val = this.scaleValue(val, true);
+            return { offset: offset, color: colorStr, value: val };
+        }
+        return false;
+    }
+
+    scaleValue(val, format = false) {
+        if (this.props.handleAs === appStrings.COLORBAR_JSON_RELATIVE) {
+            val = parseFloat(val);
+            let min = parseFloat(this.props.min) || 0;
+            let max = parseFloat(this.props.max) || 0;
+            val = min + (max - min) * val;
+            if (format) {
+                val = val.toFixed(2);
+            }
+        }
+        return val;
     }
 
     draw() {
@@ -48,6 +116,7 @@ export class ColorbarJSON extends Component {
         let height = typeof this.props.height !== "undefined" ? this.props.height : CANVAS_HEIGHT;
 
         let containerClasses = MiscUtil.generateStringFromSet({
+            [styles.hoverCross]: typeof this.props.onMouseOver !== "undefined",
             [this.props.className]: typeof this.props.className !== "undefined"
         });
 
@@ -66,6 +135,11 @@ ColorbarJSON.propTypes = {
     palette: PropTypes.object,
     width: PropTypes.number,
     height: PropTypes.number,
+    handleAs: PropTypes.string,
+    onMouseOver: PropTypes.func,
+    onMouseOut: PropTypes.func,
+    min: PropTypes.number,
+    max: PropTypes.number,
     className: PropTypes.string
 };
 
